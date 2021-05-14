7cac416ffdf24112cc7a8f895d1b4e918ad1b8ec
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-05-14 12:00:01 +0200

    add responsive text

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ document.querySelector("#button").addEventListener("click", function () {
 
 function newItem(){
 
-//javascript
 //1. Adding a new item to the list of items:
    let li = document.createElement("li");
    let inputValue = document.getElementById("input").value;
@@ -32,6 +31,8 @@ function newItem(){
  	}
 
  	li.addEventListener("dblclick",crossOut);
+  // add responsiveness
+  li.addEventListener("touchstart", crossOut);
 
  //3(i). Adding the delete button "X":
    let crossOutButton = document.createElement("crossOutButton");
@@ -47,3 +48,23 @@ function newItem(){
    $('#list').sortable();
 
 }
+
+
+// 5. in order to make it responsive, I change the instrucctions to "tab" instead of "click"
+// according to the screen size, assuming that when you use a screen larger than 700px you
+//  will be using a computer instead of a smartphone or tablet.
+
+$(document).ready(function(){
+   if($( window ).width() < 767){
+       $("p").text("Tab to mark an item off, click on 'X' to delete an item, and drag items to reorder.");
+   }else{
+       $("p").text("Double click to mark an item off, click on 'X' to delete an item, and drag items to reorder.");
+   }
+});
+$( window ).resize(function() {
+	if($( window ).width() < 767){
+       $("p").text("Tab to mark an item off, click on 'X' to delete an item, and drag items to reorder.");
+   }else{
+       $("p").text("Double click to mark an item off, click on 'X' to delete an item, and drag items to reorder.");
+   }
+});
