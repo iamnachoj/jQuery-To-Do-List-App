a132e880aaa142fea098e1749444371099fc9409
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-05-14 13:28:06 +0200

    Update code to jQuery

--- app.js
+++ app.js
@@ -9,43 +9,77 @@ $("#button").click(()=> {
 });
 
 function newItem(){
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.append(inputValue);
 
-//1. Adding a new item to the list of items:
-   let li = document.createElement("li");
-   let inputValue = document.getElementById("input").value;
-   let text = document.createTextNode(inputValue);
-   li.appendChild(text);
+  if ($("#input").val() === "") {
+      alert("Task must not be empty!")
+    } else {
+      $("#list").append(li);
+      $("#input").val("")
+    }
 
-   if (inputValue === '') {
-     alert("You must write something!");
-   } else {
-     let list = document.querySelector('#list');
-     list.appendChild(li);
-   }
+  li.on('dblclick', function crossOut() {
+      li.toggleClass('strike')
+    });
+    // responsive
+  li.on("touchstart", function crossOut() {
+    li.toggleClass('strike')
+    });
 
- //2. Crossing out an item from the list of items:
-   function crossOut() {
- 		li.classList.toggle("strike");
- 	}
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
 
- 	li.addEventListener("dblclick",crossOut);
-  // add responsiveness
-  li.addEventListener("touchstart", crossOut);
+    crossOutButton.on("click", deleteListItem);
+     function deleteListItem(){
+        li.remove();
+      }
 
- //3(i). Adding the delete button "X":
-   let crossOutButton = document.createElement("crossOutButton");
- 	crossOutButton.appendChild(document.createTextNode("X"));
- 	li.appendChild(crossOutButton);
-
- 	crossOutButton.addEventListener("click", deleteListItem);
- //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-   function deleteListItem(){
- 		li.classList.add("delete")
- 	}
- // 4. Reordering the items:
-   $('#list').sortable();
+      // 4. Reordering the items:
+     $('#list').sortable();
 
 }
+
+// function newItem(){
+//
+// //1. Adding a new item to the list of items:
+//    let li = document.createElement("li");
+//    let inputValue = document.getElementById("input").value;
+//    let text = document.createTextNode(inputValue);
+//    li.appendChild(text);
+//
+//    if (inputValue === '') {
+//      alert("You must write something!");
+//    } else {
+//      let list = document.querySelector('#list');
+//      list.appendChild(li);
+//    }
+//
+//  //2. Crossing out an item from the list of items:
+//    function crossOut() {
+//  		li.classList.toggle("strike");
+//  	}
+//
+//  	li.addEventListener("dblclick",crossOut);
+//   // add responsiveness
+//   li.addEventListener("touchstart", crossOut);
+//
+//  //3(i). Adding the delete button "X":
+//    let crossOutButton = document.createElement("crossOutButton");
+//  	crossOutButton.appendChild(document.createTextNode("X"));
+//  	li.appendChild(crossOutButton);
+//
+//  	crossOutButton.addEventListener("click", deleteListItem);
+//  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+//    function deleteListItem(){
+//  		li.classList.add("delete")
+//  	}
+//  // 4. Reordering the items:
+//    $('#list').sortable();
+//
+// }
 
 
 // 5. in order to make it responsive, I change the instrucctions to "tab" instead of "click"
